8c87b9d649268512336995fc15752f44df4f27a0
jinsu3758 <jinsu3758@naver.com>
2020-02-12 16:51:07 +0900

    CandleStick TrackerLayer 수정  - UIView의 UITouch함수에서 longPressedGestureReconizer로 변경

---
+++ CandleChartTest/CandleChartTest/CandleStickInteractiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CandleStickInteractiveViewController.swift
+//  CandleChartTest
+//
+//  Created by 박진수 on 04/02/2020.
+//  Copyright © 2020 박진수. All rights reserved.
+//
+
+import UIKit
+
+class CandleStickInteractiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CandleChartTest/CandleChartTest/ZChartCandleStickLayer.swift
@@ -0,0 +1,9 @@
+//
+//  ZChartCandleStickLayer.swift
+//  CandleChartTest
+//
+//  Created by 박진수 on 04/02/2020.
+//  Copyright © 2020 박진수. All rights reserved.
+//
+
+import Foundation
