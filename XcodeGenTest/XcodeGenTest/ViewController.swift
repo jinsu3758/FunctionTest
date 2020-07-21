55d3f03a600dec1233fe21e364298e4140e7ae0a
jinsu3758 <jinsu3758@naver.com>
2020-07-21 15:41:24 +0900

    init XcodeGen

---
+++ XcodeGenTest/XcodeGenTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenTest
+//
+//  Created by 박진수 on 2020/07/20.
+//  Copyright © 2020 박진수. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
