faf60b741cd2d7e2c04bc221539260686970d415
jinsu3758 <jinsu3758@naver.com>
2020-08-10 09:40:51 +0900

    클로저테스트 init

---
+++ ClosureTest/ClosureTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosureTest
+//
+//  Created by 박진수 on 2020/08/07.
+//  Copyright © 2020 jinsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
