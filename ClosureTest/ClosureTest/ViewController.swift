3b20e87ea4a308e05d7bae506fbdd0266351d50d
jinsu3758 <jinsu3758@naver.com>
2020-09-09 21:30:14 +0900

    GCD Test init

--- ClosureTest/ClosureTest/ViewController.swift
+++ ClosureTest/ClosureTest/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Just(5).sink { value in
+            print(value)
+        }
     }
 
 
---
+++ GCDTest.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/// sync 실험
+//let serialSyncDispatchQueue = DispatchQueue(label: "Serial Queue")
+//
+//serialSyncDispatchQueue.sync {
+//    print("1")
+//}
+//print("2")
+//
+//DispatchQueue.global().sync {
+//    print("3")
+//}
+//
+//DispatchQueue.global(qos: .background).sync {
+//    print("4")
+//}
+//
+//serialSyncDispatchQueue.sync {
+//    print("5")
+//}
+/// 결과 : 12345
+
+
+/// Serial, Concurrent
+//DispatchQueue(label: "serial 1").async {
+//    print("1")
+//}
+//DispatchQueue.global().async {
+//    print("concurrent 1")
+//}
+//
+//print("2")
+//
+//DispatchQueue.global().async {
+//    print("concurrent 1")
+//}
+//
+//DispatchQueue(label: "serail 2").async {
+//    print("3")
+//}
+//DispatchQueue(label: "serial 3").sync {
+//    print("4")
+//}
+//print("5")
+
+/// 결과 : 21435는 일정 concurrent는 그 때 그 때 다름
+
+
+/// 같은 DispatchQueue에서 sync, async해보기
+//let serialDispatchQueue = DispatchQueue(label: "Serial Queue")
+
+//serialDispatchQueue.sync {
+//    print("1")
+//}
+//
+//serialDispatchQueue.async {
+//    print("2")
+//}
+//
+//serialDispatchQueue.async {
+//    print("3")
+//}
+//
+//serialDispatchQueue.async {
+//    print("4")
+//}
+//
+//serialDispatchQueue.sync {
+//    print("5")
+//}
+//serialDispatchQueue.async {
+//    print("6")
+//}
+
+/// 결과 : 123456 순서대로 된다
+
+
+let serialDispatchQueue = DispatchQueue(label: "Serial Queue")
+let concurrentDispatchQueue = DispatchQueue.global()
+
+
+serialDispatchQueue.async {
+    print("1")
+}
+
+concurrentDispatchQueue.async {
+    print("2")
+}
+
+
